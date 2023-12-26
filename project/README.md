b63caea6a63628b5d60771763e6d1a7b516db9a6
Anjali Kaithakkal <anjalijohnk@gmail.com>
2023-12-26 13:23:18 +0530

    uploaded project

---
+++ project/README.md
@@ -0,0 +1,94 @@
+# Stock Analysis Script
+
+## Overview
+
+This Python script performs analysis on top companies listed on the Bombay Stock Exchange (BSE), India. It fetches data from the BSE website to obtain a list of the top companies and then utilizes the Alpha Vantage API for technical analysis, including Exponential Moving Averages (EMA), Relative Strength Index (RSI), Moving Average Convergence Divergence (MACD), and Signal Line.
+
+## Table of Contents
+
+- [Requirements](#requirements)
+- [Configuration](#configuration)
+- [Installation](#installation)
+- [Script Explanation](#script-explanation)
+- [Notes](#notes)
+- [Disclaimer](#disclaimer)
+
+## Requirements
+
+- Python 3.x
+- Libraries: `requests`, `pandas`, `beautifulsoup4`, `tabulate`
+
+## Configuration
+
+Before running the script, ensure that you have an Alpha Vantage API key. You can obtain a free key by signing up [here](https://www.alphavantage.co/support/#api-key). Replace the placeholder `ALPHA_VANTAGE_KEY` in the script with your actual API key.
+
+Additionally, provide your user agent for web scraping. You can find your user agent [here](https://www.whatismybrowser.com/). Update the `USER_AGENT` variable with your user agent.
+
+## Installation
+
+1. Install the required Python libraries:
+
+   ```bash
+   pip install requests pandas beautifulsoup4 tabulate
+
+2. Run the Script
+
+To execute the stock analysis script, open your terminal or command prompt and navigate to the directory where the script (`project.py`) is located. Then, run the following command:
+
+```bash
+python project.py
+
+## Script Explanation
+
+The script performs the following steps:
+
+- **Fetches the list of top companies from the BSE website using web scraping techniques.**
+  - Utilizes the BeautifulSoup library to parse HTML and extract relevant information.
+  - Identifies company names, symbols, and closing prices.
+
+- **Retrieves various technical indicators for the top companies using the Alpha Vantage API, including EMA, RSI, MACD, and Signal Line.**
+  - Makes API requests to Alpha Vantage for each technical indicator.
+  - Gathers data and stores it in a pandas DataFrame for analysis.
+
+- **Filters the companies based on specific criteria, such as RSI greater than 50, closing price above certain EMAs, and MACD values.**
+  - Implements conditional statements to determine if a company meets the specified criteria.
+  - Uses technical indicators to assess the performance of each company.
+
+- **Prints the top 3 companies that meet the criteria in a tabular format.**
+  - Utilizes the tabulate library to present the information in a clear and organized table.
+  - Displays relevant details such as company name, symbol, closing price, and various technical indicators.
+
+This comprehensive analysis provides insights into the performance of top companies based on a combination of fundamental and technical factors.
+
+## Notes
+
+- **Data Sensitivity:**
+  - Acknowledge that the financial markets can be unpredictable, and the script's analysis is based on historical data and algorithms. Emphasize that past performance is not indicative of future results.
+
+- **API Key Security:**
+  - Emphasize the importance of keeping the Alpha Vantage API key secure. Avoid sharing it publicly or embedding it directly in the script if possible. Consider using environment variables or a configuration file for added security.
+
+- **User Instructions:**
+  - Provide clear instructions on how users can interpret the results presented by the script. For example, explain the significance of the technical indicators and how users might use them in their decision-making process.
+
+- **Customization:**
+  - Encourage users to explore the script and consider customizing it based on their specific needs. This could involve modifying the technical indicators used, adjusting criteria for company selection, or incorporating additional data sources.
+
+- **Logging:**
+  - Consider adding logging functionality to the script. This can help users troubleshoot any issues and provide a record of script execution, including timestamps and any errors encountered.
+
+- **Dependency Versions:**
+  - Specify the versions of the libraries used in the script. This can help users ensure compatibility and avoid potential issues arising from library updates.
+
+- **Contributions and Support:**
+  - Invite users to contribute to the script or report issues on a platform like GitHub. Provide information on how users can seek support or contribute to the script's development.
+
+## Disclaimer
+
+This script is for educational and informational purposes only. It does not constitute financial advice. The analysis and results obtained from the script are based on historical data and algorithmic calculations, which may not accurately predict future market conditions.
+
+Always do your own research and consult with a qualified financial professional before making any investment decisions. The authors and contributors of this script are not responsible for any financial losses or gains that may result from the use of this script. Use it at your own risk.
+
+Please comply with the terms of use of external services such as Alpha Vantage and BSE to ensure ethical and legal usage of their data.
+
+
---
+++ project/project.py
@@ -0,0 +1,186 @@
+
+
+import re
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+
+
+# Constants
+# The following url provides the list of top companies listed on the Bombay Stock Exchange (BSE), India
+BSE_URL = "https://www.bseindia.com/markets/equity/EQReports/TopMarketCapitalization.aspx"
+ALPHA_VANTAGE_URL = 'https://www.alphavantage.co/query?'
+# use your Alpha Vantage API key here; sign up for a free API key: https://www.alphavantage.co/support/#api-key
+ALPHA_VANTAGE_KEY = 'X2187U7FG6Y809IZ'
+# Retrieve your user agent from https://www.whatismybrowser.com/. Use the user agent and include a header
+# declaration in your Python script for web scraping. This helps prevent your activity from being classified
+# as spam and potentially blacklisted by the target website.
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:120.0) Gecko/20100101 Firefox/120.0'
+
+
+
+# Function to fetch BSE100 companies name and symbol from the BSE site
+def fetch_bse_top_comapnies_list(url, user_agent):
+    """
+    Fetches BSE100 companies' names, symbols, and closing prices from the BSE site.
+
+    Args:
+    url (str): URL of the BSE page.
+    user_agent (str): User agent for the HTTP request.
+
+    Returns:
+    List[Dict]: List of dictionaries containing company information.
+    """
+
+    headers = {'User-Agent': user_agent}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    anchor_tags = soup.select('td.tdcolumn a.tablebluelink')
+
+    pattern = r'/([^/]+)/(?:[^/]+)/$'
+    tag = re.compile(rf'ContentPlaceHolder1_gvData_lblTurnover_\d+')
+
+
+    company_info = []
+    for anchor in anchor_tags:
+
+        closing_price_td = anchor.find_next('td', {'class': 'tdcolumn text-right'})
+        closing_price = float(closing_price_td.find('span', {'id': tag}).text.strip())
+
+        # Append the data to the list
+        company_info.append({
+            'company_name': anchor.text.strip(),
+            'symbol': re.search(pattern, anchor['href']).group(1).upper(),
+            'closing_price': closing_price
+        })
+
+    return company_info
+
+
+
+# Function to fetch EMA data from Alpha Vantage
+def fetch_ema(url, symbol, api_key, time_period, series_type='close'):
+    params = {
+        'function': 'EMA',
+        'symbol': symbol,
+        'interval': 'daily',
+        'time_period': time_period,
+        'series_type': series_type,
+        'apikey': api_key
+    }
+
+    # Make the API request
+    response = requests.get(url, params=params)
+    # Parse the response and extract the data
+    data = response.json()
+    # Convert the data to a pandas DataFrame
+    df = pd.DataFrame(data['Technical Analysis: EMA']).T.astype(float)
+
+    # return the DataFrame
+    return df
+
+
+
+
+# Function to fetch RSI data from Alpha Vantage
+def fetch_rsi(url, symbol, api_key, time_period, series_type='close'):
+    params = {
+        'function': 'RSI',
+        'symbol': symbol,
+        'interval': 'daily',
+        'time_period': time_period,
+        'series_type': series_type,
+        'apikey': api_key
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    # Convert the data to a pandas DataFrame
+    df = pd.DataFrame(data['Technical Analysis: RSI']).T.astype(float)
+    # Extract the latest RSI value
+    current_rsi = df.iloc[0]['RSI']
+    return current_rsi
+
+
+
+# Function to calculate MACD and Signal Line
+def calculate_macd_and_signal_line(url, symbol, api_key):
+    """
+    Calculate Moving Average Convergence Divergence (MACD) and Signal Line.
+
+    Args:
+    base_url (str): The base URL for Alpha Vantage API.
+    symbol (str): The stock symbol.
+    api_key (str): Alpha Vantage API key.
+
+    Returns:
+    tuple: Latest MACD value and latest Signal Line value.
+    """
+    ema_12 = fetch_ema(url, symbol, api_key, time_period=12)
+    ema_26 = fetch_ema(url, symbol, api_key, time_period=26)
+
+    merged_df = pd.merge(ema_12, ema_26, left_index=True, right_index=True, suffixes=('_ema12', '_ema26'))
+    # Calculate MACD
+    merged_df['MACD'] = merged_df['EMA_ema12'] - merged_df['EMA_ema26']
+    latest_macd_value = round(merged_df['MACD'].iloc[0], 2)
+
+    merged_df['Signal_Line'] = merged_df['MACD'].ewm(span=9, adjust=False).mean()
+    latest_signal_value = round(merged_df['Signal_Line'].iloc[0], 2)
+
+    return latest_macd_value, latest_signal_value
+
+
+# Main function
+def main():
+    """
+    Main function to fetch BSE top companies, analyze their indicators, and print the top 3 companies.
+    """
+    companies_info = fetch_bse_top_comapnies_list(BSE_URL, USER_AGENT)
+    top_companies = []
+
+    for company in companies_info[:5]:
+        symbol = f"{company['symbol']}.BSE"
+
+        ema_20       = fetch_ema(ALPHA_VANTAGE_URL, symbol, ALPHA_VANTAGE_KEY, time_period=20).iloc[0]['EMA']
+        ema_50       = fetch_ema(ALPHA_VANTAGE_URL, symbol, ALPHA_VANTAGE_KEY, time_period=50).iloc[0]['EMA']
+        #ema_200     = fetch_ema(ALPHA_VANTAGE_URL, symbol, ALPHA_VANTAGE_KEY, time_period=200).iloc[0]['EMA']
+
+        current_rsi  = fetch_rsi(ALPHA_VANTAGE_URL, symbol, ALPHA_VANTAGE_KEY, time_period=14)
+
+        macd_value, signal_line_value = calculate_macd_and_signal_line(ALPHA_VANTAGE_URL, symbol, ALPHA_VANTAGE_KEY)
+
+        
+        closing_price = company['closing_price']
+
+        if (
+            current_rsi > 50 and
+            closing_price > ema_20 and
+            closing_price > ema_50 and
+            closing_price > max(ema_20, ema_50) * 1.01 and
+            macd_value > signal_line_value
+        ):
+            top_companies.append({
+                'company_name': company['company_name'],
+                'symbol': company['symbol'],
+                'closing_price': closing_price,
+                'ema_20': ema_20,
+                'ema_50': ema_50,
+                'rsi': current_rsi,
+                'macd': macd_value,
+                'signal_line': signal_line_value
+            })
+
+    top_companies.sort(key=lambda x: x['rsi'], reverse=True)
+    top3_companies = top_companies[:3]
+
+    #print info of the top 3 companies from the first five companies on the BSE100 list
+    print(tabulate(top3_companies, headers='keys'))
+
+
+
+if __name__ == "__main__":
+    main()
